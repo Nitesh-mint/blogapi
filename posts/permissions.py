58f8b460e43a3d1ba9492631a59f1ba0d6115cf8
Nitesh-mint <nitesh.raya058@gmail.com>
2022-11-01 08:33:43 +0545

    added custom permissions !

---
+++ posts/permissions.py
@@ -0,0 +1,17 @@
+from urllib import request
+from rest_framework import permissions
+
+class IsAuthorOrReadOnly(permissions.BasePermission):
+    def has_permission(self,request,view):
+        #authenticated users can only see List view
+        if request.user.is_authenticated:
+            return True
+        return False
+    
+    def has_object_permission(self,request,view,obj):
+        #read permission are allowed to anyone
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        
+        #write permissions are only allowed to the author of the post\
+        return obj.author == request.user
